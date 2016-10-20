a11d397db1cda17ed4c83c35f793bdfa90801709
Markus Schmidl <markus.schmidl@ymail.com>
2016-10-20 17:47:59 +0200

    now getting the right can id

--- src/lib/hausbus_protocols.c
+++ src/lib/hausbus_protocols.c
@@ -1,3 +1,6 @@
+#include <avr/pgmspace.h>
+#include <mcp2515.h>
+
 #include "hausbus_protocols.h"
 
 const struct {
@@ -12,6 +15,7 @@ const struct {
 void hausbus_handler(void)
 {
 	uint8_t i;
+	uint32_t addr;
 
 	for (i=0; i<sizeof(hausbus_handlermap)/sizeof(*hausbus_handlermap); i++) {
 		/*
@@ -19,7 +23,8 @@ void hausbus_handler(void)
 		 * it's position is dependent on the frame type
 		 * it will be checked in each handler function
 		 */
-		if (0 == memcmp_P(&can_frame.addr, &hausbus_handlermap[i].id, 4)) {
+		addr = can_addr & (MCP2515_RX_EID_FLAG | MCP2515_RX_ID_MASK | (can_is_extended?(MCP2515_RX_EID_MASK):0));
+		if (0 == memcmp_P(&addr, &hausbus_handlermap[i].id, 4)) {
 			hausbus_handlermap[i].handler();
 			break;
 		}
