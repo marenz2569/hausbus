caa9aa444c27728fc0b17ed3bf30122535d0023b
Markus Schmidl <markus.schmidl@ymail.com>
2017-05-28 05:12:41 +0200

    removed examples

--- src/dos.h
+++
@@ -1,8 +0,0 @@
-#ifndef DOS_H__
-#define DOS_H__
-
-#define SWITCH_TABLE
-
-#define PWM_TABLE
-
-#endif
--- src/lamp.h
+++
@@ -1,9 +0,0 @@
-#ifndef LAMP_H__
-#define LAMP_H__
-
-#define PWM_TABLE \
-	ENTRY(B, 1, 101, 1)
-
-#define SWITCH_TABLE
-
-#endif
